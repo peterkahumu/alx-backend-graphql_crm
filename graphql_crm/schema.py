6cbe056b501ce3ab5ec73804db28a77270ffc8e9
peterkahumu <muhumukip@gmail.com>
2025-06-28 16:42:09 +0300

    Added the graphql folder

---
+++ graphql_crm/schema.py
@@ -0,0 +1,12 @@
+# graphql_crm/schema.py
+import graphene
+from crm.schema import Query as CRMQuery
+from crm.schema import Mutation as CRMMutation
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
